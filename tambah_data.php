d1de2bd892e4bb863183a5df18c736d0c4426b8b
ikanofalia <nofaliaika0@gmail.com>
2025-11-04 15:20:53 +0700

    Menambahkan file tambah_data.php

--- tambah_data.php
+++ tambah_data.php
@@ -1,1 +1,21 @@
-<?php echo "Data berhasil ditambahkan!"; ?>
+<?php
+include 'koneksi.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nama = $_POST['nama'];
+    $email = $_POST['email'];
+
+    $sql = "INSERT INTO pengguna (nama, email) VALUES ('$nama', '$email')";
+    if ($conn->query($sql) === TRUE) {
+        echo "Data berhasil ditambahkan!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+?>
+
+<form method="POST" action="">
+    Nama: <input type="text" name="nama" required><br>
+    Email: <input type="email" name="email" required><br>
+    <input type="submit" value="Tambah Data">
+</form>
