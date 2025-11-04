098020d52e570d2aa3fb3880bde4f4fa7bc3c113
ikanofalia <nofaliaika0@gmail.com>
2025-11-04 15:27:13 +0700

    Menambahkan koneksi.php untuk koneksi database

--- koneksi.php
+++ koneksi.php
@@ -1,1 +1,15 @@
-<?php echo "Koneksi berhasil!"; ?>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db_latihan";
+
+// Membuat koneksi
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Cek koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+echo "Koneksi berhasil!";
+?>
